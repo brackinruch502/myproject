13eb6bbc95b88d39fdc5a7831bda7b72a19af032
brackinruch502 <brackinruch502@gmail.com>
2025-03-29 19:30:43 +0000

    Add 19.py

---
+++ src/19.py
@@ -0,0 +1,11 @@
+import math
+
+def calculate_volume(length, width, height):
+    volume = length * width * height
+    return volume
+
+length = 5.0
+width = 3.0
+height = 2.0
+volume = calculate_volume(length, width, height)
+print("The volume is:", volume)
