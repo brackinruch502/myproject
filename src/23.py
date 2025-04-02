1c625243e59003a542b450f8542c98fb843373f8
brackinruch502 <brackinruch502@gmail.com>
2025-04-02 11:01:32 +0000

    Add 23.py

---
+++ src/23.py
@@ -0,0 +1,8 @@
+def greet_user(name: str) -> None:
+    """
+    This function greets a user by name.
+    :param name: The name of the user to greet.
+    """
+    print(f"Hello, {name}!")
+
+greet_user("Alice")
