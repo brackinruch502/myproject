0e0ad35e78dedba4a497849d7e12cf09be410329
brackinruch502 <brackinruch502@gmail.com>
2025-04-08 05:12:27 +0000

    Add 27.py

---
+++ src/27.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+def print_hello_world():
+    # This is just a placeholder function to demonstrate how you can interact with your project's environment.
+    print("Hello World!")
+
+print_hello_world()
