7481fcac6e8a3f81708a57a47703af15e0e6bf6a
brackinruch502 <brackinruch502@gmail.com>
2025-04-11 12:48:28 +0000

    Add 60.py

---
+++ src/60.py
@@ -0,0 +1,5 @@
+import os
+os.system("git clone https://github.com/username/myproject.git")
+os.chdir("myproject")
+os.system("pip install --upgrade pip")
+os.system("pip install -r requirements.txt")
