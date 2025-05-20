b5956420b4b78a1cea6459a4c682c43b9660f0aa
brackinruch502 <brackinruch502@gmail.com>
2025-05-20 08:39:28 +0000

    Add 82.py

---
+++ src/82.py
@@ -0,0 +1,8 @@
+def add_numbers(a: float, b: float) -> float:
+    return a + b
+
+if __name__ == "__main__":
+    num1 = 5.0
+    num2 = 3.0
+    result = add_numbers(num1, num2)
+    print(result)
